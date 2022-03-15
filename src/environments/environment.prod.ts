7e624a91e1bed9fe75fab7775cbaa2e508a0c657
Axl <makosful@gmail.com>
2022-03-15 21:32:30 +0100

    feat: Add production environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  keycloakUrl: 'http://localhost:8010/',
+  keycloakRealm: 'angular-web',
+  keycloakClientId: 'angular-web-client',
+  graphqlApi: 'http://localhost:8080/'
 };
